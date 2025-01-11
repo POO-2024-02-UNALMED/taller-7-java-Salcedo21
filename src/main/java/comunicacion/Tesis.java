07ae6ea72360766600fb9a809a5491e52ed95757
Santiago Salcedo <ssalcedor@unal.edu.co>
2025-01-10 19:17:49 -0500

    4

--- src/main/java/comunicacion/Tesis.java
+++ src/main/java/comunicacion/Tesis.java
@@ -55,7 +55,7 @@ public class Tesis extends Escrito{
     }
 
     @Override
-    int palabrasTotales(int palabrasPagina) {
+    public int palabrasTotales(int palabrasPagina) {
         return palabrasPagina * 5 * getPaginas();
     }
 
